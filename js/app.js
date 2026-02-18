a6bbf14ed11523faa250aa42e7b616024f855bdc
Drake-Zerep <drakezerep@gmail.com>
2026-02-18 17:44:28 +0000

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+fetch("data/noticias.json")
+  .then(res => res.json())
+  .then(noticias => {
+    const contenedor = document.getElementById("noticias");
+
+    noticias.forEach(noticia => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <h3>${noticia.titulo}</h3>
+        <small>${noticia.fecha}</small>
+        <p>${noticia.contenido}</p>
+      `;
+
+      contenedor.appendChild(card);
+    });
+  });
