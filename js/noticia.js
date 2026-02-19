43744391920112db64d93e624d1100704b4ec20f
Drake-Zerep <drakezerep@gmail.com>
2026-02-19 01:36:52 +0000

    Create noticia.js

---
+++ js/noticia.js
@@ -0,0 +1,22 @@
+// Obtener el slug de la URL
+const params = new URLSearchParams(window.location.search);
+const slug = params.get("slug");
+
+fetch("data/noticias.json")
+  .then(res => res.json())
+  .then(noticias => {
+    const noticia = noticias.find(n => n.slug === slug);
+    const contenedor = document.getElementById("contenido-noticia");
+
+    if (!noticia) {
+      contenedor.innerHTML = "<h2>Noticia no encontrada</h2>";
+      return;
+    }
+
+    contenedor.innerHTML = `
+      <h1>${noticia.titulo}</h1>
+      <small>${noticia.fecha} — ${noticia.categoria.toUpperCase()}</small>
+      <p>${noticia.contenido}</p>
+      <a href="index.html" class="volver">← Volver</a>
+    `;
+  });
